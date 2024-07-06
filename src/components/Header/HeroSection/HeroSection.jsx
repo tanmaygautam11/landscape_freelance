b348f88b572f28e099894bf98adfc83c3779927c
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-06 15:34:41 +0530

    carousel done

--- src/components/Header/HeroSection/HeroSection.jsx
+++ src/components/Header/HeroSection/HeroSection.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./HeroSection.css";
+import arrow from "../../../assets/arrow.svg";
 
 const HeroSection = () => {
   const images = [
     "https://images.pexels.com/photos/914682/pexels-photo-914682.jpeg?auto=compress&cs=tinysrgb",
-    "https://images.pexels.com/photos/919317/pexels-photo-919317.jpeg?auto=compress&cs=tinysrgb",
-    "https://images.pexels.com/photos/1084380/pexels-photo-1084380.jpeg?auto=compress&cs=tinysrgb",
+    "https://images.pexels.com/photos/116910/pexels-photo-116910.jpeg?auto=compress&cs=tinysrgb",
+    "https://images.pexels.com/photos/7174108/pexels-photo-7174108.jpeg?auto=compress&cs=tinysrgb",
   ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const customRenderArrowNext = (onClickHandler, hasNext, label) => {
+    return (
+      hasNext && (
+        <button className="custom-arrow" onClick={onClickHandler} title={label}>
+          <img src={arrow} alt="forward-arrow.svg" />
+        </button>
+      )
+    );
+  };
 
   return (
     <div className="hero-container">
@@ -17,7 +29,9 @@ const HeroSection = () => {
         infiniteLoop={true}
         autoPlay={true}
         interval={5000}
-        showStatus={true}
+        showStatus={false}
+        onChange={(index) => setCurrentSlide(index)}
+        renderArrowNext={customRenderArrowNext}
       >
         {images.map((url, index) => (
           <div key={index} className="hero-slide">
@@ -30,8 +44,11 @@ const HeroSection = () => {
       </Carousel>
       <div className="hero-overlay"></div>
       <div className="hero-text">
-        <h1>I am John Doe</h1>
-        <button>Hire me</button>
+        <h1>We Bring Your Landscape to Life.</h1>
+        <button>VIEW PROJECT</button>
+      </div>
+      <div className="carousel-status">
+        {currentSlide + 1} / {images.length}
       </div>
     </div>
   );
