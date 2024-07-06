53cc725b53447b0462f880f0640cf2e92d343a04
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-06 16:22:46 +0530

    forward arrow btn animated

--- src/components/Header/HeroSection/HeroSection.jsx
+++ src/components/Header/HeroSection/HeroSection.jsx
@@ -11,10 +11,22 @@ const HeroSection = () => {
     "https://images.pexels.com/photos/7174108/pexels-photo-7174108.jpeg?auto=compress&cs=tinysrgb",
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
+
   const customRenderArrowNext = (onClickHandler, hasNext, label) => {
+    const handleClick = () => {
+      onClickHandler();
+      const arrowBtn = document.querySelector(".custom-arrow");
+      if (arrowBtn) {
+        arrowBtn.classList.add("clicked");
+        setTimeout(() => {
+          arrowBtn.classList.remove("clicked");
+        }, 300);
+      }
+    };
+
     return (
       hasNext && (
-        <button className="custom-arrow" onClick={onClickHandler} title={label}>
+        <button className="custom-arrow" onClick={handleClick} title={label}>
           <img src={arrow} alt="forward-arrow.svg" />
         </button>
       )
