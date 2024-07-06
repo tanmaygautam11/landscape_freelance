578c8628ebd8d3b56e0451202a4408615d061248
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-06 23:55:07 +0530

    social media done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,22 @@
 import "./Footer.css";
+import facebook from "../../assets/socials/facebook.png";
+import twitter from "../../assets/socials/twitter.png";
+import instagram from "../../assets/socials/instagram.png";
+import linkedin from "../../assets/socials/linkedin.png";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <div className="social-media">
+        <h2>SOCIAL MEDIA</h2>
+        <p>Follow us on Social Media</p>
+        <div className="icons">
+          <img src={facebook} alt="facebook" />
+          <img src={twitter} alt="twitter" />
+          <img src={instagram} alt="instagram" />
+          <img src={linkedin} alt="linkedin" />
+        </div>
+      </div>
     </div>
   );
 };
