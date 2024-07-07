b283e56ddc4a4ed616ee1d2ee34c9b43aa35be90
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-07 23:42:41 +0530

    footer final

--- src/components/footer/Location/Location.jsx
+++ src/components/footer/Location/Location.jsx
@@ -1,36 +1,44 @@
-import "./Location.css";
+import "./Location.css"; // Make sure this import is at the top
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 const Location = () => {
   return (
     <div className="location-container">
-      <div className="address">
-        <h2> Baten Construction and Landscaping LLC</h2>
-        <p>2611 Kirtland Ave.</p>
-        <p>Forestville, MD 20747</p>
-        <p>United States</p>
-        <div className="contact-link">
-          <p>
-            <strong>Phone:</strong> 301-915-5370
-          </p>
-          <p>
-            <strong>Email: </strong>
-            <a href="mailto:">
-              <u>info@lawnandlandscaping.us</u>
-            </a>
-          </p>
+      <div className="footer-div">
+        <div className="address">
+          <h2> Baten Construction and Landscaping LLC</h2>
+          <p>407 Main St</p>
+          <p>Laurel, MD 20707</p>
+          <p>United States</p>
+          <div className="contact-link">
+            <p>
+              <strong>Phone:</strong> 301-915-5370
+            </p>
+            <p>
+              <strong>Email: </strong>
+              <a href="mailto:info@lawnandlandscaping.us">
+                <u>info@lawnandlandscaping.us</u>
+              </a>
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="footer-icons">
+          <FacebookRoundedIcon sx={{ color: "#fff", fontSize: 42 }} />
+          <XIcon sx={{ color: "#fff", fontSize: 37 }} />
+          <InstagramIcon sx={{ color: "#fff", fontSize: 42 }} />
+          <LinkedInIcon sx={{ color: "#fff", fontSize: 42 }} />
         </div>
       </div>
-      <hr />
-
-      <div className="footer-icons">
-        <FacebookRoundedIcon sx={{ color: "#fff", fontSize: 42 }} />
-        <XIcon sx={{ color: "#fff", fontSize: 37 }} />
-
-        <InstagramIcon sx={{ color: "#fff", fontSize: 42 }} />
-        <LinkedInIcon sx={{ color: "#fff", fontSize: 42 }} />
+      <div className="rights-reserved">
+        <hr />
+        <p>
+          &copy; {new Date().getFullYear()} Baten Construction and Landscaping
+          LLC. All rights reserved.
+        </p>
       </div>
     </div>
   );
