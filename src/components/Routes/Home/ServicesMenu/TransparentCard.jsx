af2342bef5f9681c0034e2ad9531be6bfe8f29c2
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-09 22:09:00 +0530

    service menu completed

--- src/components/Routes/Home/ServicesMenu/TransparentCard.jsx
+++ src/components/Routes/Home/ServicesMenu/TransparentCard.jsx
@@ -4,7 +4,9 @@ import "./TransparentCard.css";
 const TransparentCard = ({ imageSrc, price, description, buttonText }) => {
   return (
     <div className="transparent-card">
-      <img src={imageSrc} alt={price} className="card-image" />
+      <div className="image-container">
+        <img src={imageSrc} alt={price} className="card-image" />
+      </div>
       <p className="card-description">{description}</p>
       <h4 className="card-price">{price}</h4>
       <button className="card-button">{buttonText}</button>
