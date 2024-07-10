0e9418f2a0785466e3bc409729f3064a6a7c39b8
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-11 00:58:42 +0530

    about completed

--- src/components/Routes/About/About.jsx
+++ src/components/Routes/About/About.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./About.css";
 
 const About = () => {
@@ -12,31 +13,154 @@ const About = () => {
         />
       </div>
       <div className="about-container">
+        <h2 className="about-heading">Baten Construction & Landscaping</h2>
         <h1>About</h1>
         <div className="company-section">
           <h2>COMPANY</h2>
-          <img
-            src="https://www.floresartscape.com/upload/large/large_630dc611e13355619a435f81_3c56f9d8336f6d42f519f1b7222a0aa30e906aabb82e811fdbf0d59882a9bf17_630dc61f.webp"
-            alt="Plants"
-            className="about-image"
-          />
-          <p>
-            <strong>Flores Artscape</strong> was established in 2004 by husband
-            and wife team Rumaldo and Miriam Flores after over 25 years of prior
-            experience in landscaping and gardening. It started as part-time
-            work and has developed into a full-time commitment to our customers.
-            Our team is skilled in a variety of trades such as hardscape,
-            softscape, planting, lighting, irrigation, flagstone, pavers, and
-            much more.
-          </p>
-          <p>
-            We take our business personally. We want each and every customer to
-            fall in love with their landscape. It's important to us that our
-            customer service is perfect and that each landscape reflects the
-            unique individuals that hire us—you! Our goal is not “money in the
-            bank,” but
-            <strong>satisfied customers with projects of high quality</strong>.
-          </p>
+          <div className="company-content">
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEijDRbZIRBX9Y_72M-CTcNbVdXJRvV3yj77Kszw2UbpcOfIYL5eoHbv80P6z0U2Zna9iXCFQeizzxm2ntniCWdNDDHOcX-Wf53WsJOxPmWzxlqpuLPeqlJr8MaJCvR8HRfI2niVWgyNjn8/s1600/IMG_7569.jpg"
+              alt="company"
+              className="about-image"
+            />
+            <div className="company-text">
+              <p>
+                <strong>Mission: </strong>
+                "To constantly exceed each customer's expectations while setting
+                the industry example for quality craftsmanship and
+                professionalism."
+              </p>
+              <p>
+                At<strong> Baten Construction and Landscaping</strong>, we are
+                committed to providing the highest quality landscaping services
+                to our customers. We are a full-service landscaping company that
+                handles everything from design to installation to maintenance.
+              </p>
+              <p>
+                Our team of professionals is dedicated to providing the best
+                service possible. We take pride in our work and strive to exceed
+                our customers' expectations. Our goal is to create beautiful
+                outdoor spaces that our customers can enjoy for years to come.
+              </p>
+              <p>
+                Working as a team with our customers enables us to bring life to
+                their landscape dreams while building lasting relationships
+                based on trust and confidence in our services.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="company-section">
+          <h2>Kelly Baten, owner </h2>
+          <div className="company-content">
+            <div className="company-text">
+              <p>
+                Meet <strong>Kelly Baten</strong>, the owner of Baten
+                Construction and Landscaping and a key part of our management
+                team. With over 5 years of experience in construction and
+                landscaping, Kelly founded our company in 2013.
+              </p>
+              <p>
+                Kelly works incredibly hard and is one of the most dedicated
+                women you'll ever meet. She pays close attention to every detail
+                and takes pride in her work. Outside of work, Kelly enjoys
+                reading, volunteering at her church, and sharing her projects on
+                social media.
+              </p>
+              <p>
+                This summer, Kelly is excited to learn how to drive a truck.
+                We're fortunate to have such a passionate leader like her on our
+                team!
+              </p>
+            </div>
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj27CCvukuh_uuvLhRker0WU7dfbGNvyiCEcA3bcLkLSUeiw1uzVuiqCSxXglmjGDTy0y_1O8o2UaWxlzCD9O8CGM-CqRlWQ68PQFE7d0CEuG_3_DKo73YtDefoLUnelnR6ByT1tgILpA8/s1200/IMG_7565.jpg"
+              alt="Kelly Baten"
+              className="about-image"
+            />
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="company-section">
+          <h2>Erwin Baten, Managing Director</h2>
+          <div className="company-content">
+            <div className="company-text">
+              <p>
+                I am <strong>Erwin Baten</strong>, the Managing Director of
+                Baten Construction and Landscaping. Since 1995, I've been
+                immersed in landscaping, learning every aspect of the business
+                from the ground up. My company operates on core values of hard
+                work, attention to detail, and adaptability to meet and exceed
+                our clients' expectations.
+              </p>
+              <p>
+                Personally overseeing every project, I've assembled a team of
+                highly-trained professionals dedicated to delivering top-notch
+                workmanship. Our goal is to create stunning landscapes that
+                align with our clients' visions and budgets, maintaining the
+                highest standards of craftsmanship.
+              </p>
+              <p>
+                Located in Montgomery County, we serve clients in Bethesda,
+                Chevy Chase, Potomac, Kensington, Rockville, Wheaton, and
+                surrounding areas. Fully bonded and insured, we prioritize
+                client protection and satisfaction, ensuring peace of mind
+                throughout every job.
+              </p>
+              <p>
+                Whether you need landscape installation, design, or ongoing
+                maintenance, we're here to help bring your outdoor spaces to
+                life.
+              </p>
+            </div>
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiGCXSoDl1LmUbAU94bGsAOiXFWs-KvieHVn5z83if6AuIySrVOM-eamTxVjJFSgYDTzB4rvdUss_PLFy8nbK4pannalSmauYPb2zl67ltmCwhWjPjdCiZqgbE5-qSekVg5GiTokgNBQ1E/s1200/IMG_7496.JPG"
+              alt="erwin baten"
+              className="about-image"
+            />
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="company-section">
+          <h2>Welcome to Baten Construction and Landscaping Services </h2>
+          <div className="company-content">
+            <div className="company-text">
+              <p>
+                Baten Construction and Landscaping is a full-service outdoor
+                maintenance and installation company. We offer high-quality lawn
+                care, landscaping, hardscaping, fencing, and snow and ice
+                removal for both commercial and residential customers.
+              </p>
+              <p>
+                Since 2010, Erwin and Kelly Baten, along with their team of
+                professionals, have been helping business owners and individuals
+                enhance their outdoor spaces in the greater Washington DC area.
+                We are fully insured and bonded, not just to meet legal
+                requirements, but to ensure the safety and protection of your
+                family and our employees.
+              </p>
+
+              <p>
+                At Baten Construction and Landscaping, we take your yard
+                seriously. We use only the highest quality materials and
+                workmanship for every job, no matter the size. Our comprehensive
+                services, from design to ongoing maintenance, make us the top
+                one-stop landscaping company in the area.
+              </p>
+            </div>
+            <img
+              src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhwZFzNX97WsJKcvPDrxCZallAJsdJYSIiqWxmBj5eoPVy8VTRN8ZEPZLnm6hYTYKRHeoX9eNCrpTdectwYvA7TjcB7T44_mAWjajtjGyB4M-EmRSf4NYbYmPaaZ58bs5wo8Xgmgqqh_rQ/s640/IMG_7853.jpg"
+              alt="team"
+              className="about-image"
+            />
+          </div>
         </div>
       </div>
     </div>
