8fcfeb67e9769c9d0b3bc027b8b24c8df0424508
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-11 14:32:09 +0530

    tips date filled

--- src/components/Routes/Tips/tipsData.js
+++ src/components/Routes/Tips/tipsData.js
@@ -19,7 +19,7 @@ But there is never any waste in nature. Everything that dies becomes food for so
 <br/>Want to find out how you can grow fabulous compost, even if you don’t have much space (and without creating bad smells, offending the neighbors, or attracting unwanted critters!)? Expert gardening teacher, Stacey Murphy, joined me to create a powerful video walking you through the simplest steps to get anyone and everyone turning food scraps into healthy new soil gold! And this is just ONE of the 15 expert video lessons included in the brand new IMPACT KIT. Best of all, you can get it right now on super sale — but only during our global premiere event.
 `,
     image:
-      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh2aUWPOyzpUEpmfCwSs7dZAy9D6L1qlVinHhKn_3Bp9j7hHTPKPREg8lQIaTf27rCs7amQ9BQTHG3rvTuP6IlXpQkwLnC_WcimU0AbWbkzEDPoBufgcu_7lwTF_IuokPwKa6SIiq6nudY/s320/Agriculture_Soil_Soil_Image%255B1%255D.jpg",
+      "https://eu-images.contentstack.com/v3/assets/bltdd43779342bd9107/bltd881b894782db96e/639a08125421605b2189346e/soil-organic-matter-623884232_0.jpg",
   },
   {
     id: 2,
@@ -52,6 +52,102 @@ Your County Cooperative Extension office can provide you with soil testing kits 
     image:
       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnkbCddwaEpJUpf2Ip5gdll8FJAa0-eM4zooyaY-_43Y6DEZqG4K3y_jz0aZecckWrI1YmzJk8YuLLvCvG8IovMzjMwzDuvWbxWcONqCo32Cmv_P-dbueCwpzcXWVttHb5hy5VFoe_MY8/s640/grass-to-garden-4.jpg",
   },
+  {
+    id: 4,
+    title: "  Preparing the site to plant new grass ",
+    content: `
+A healthy, attractive lawn starts with proper site preparation.
+<br/>
+Grading of the site prior to planting is important, as it will help ensure water retention and allow for easy mowing. The University of Maryland Extension advises sloping the lawn area away from buildings at a rate of 1 to 2 percent.2 Avoid steep slopes, as they tend to dry out, and make sure the area is level. Depressions create wet spots that are hard to mow and prone to disease.
+<br/>
+If you intend to replace the entire lawn, it's important to do a thorough job of removing the old turf. Use a sod cutter to take out the old grass at the roots. Another option for ridding the area of old turf is to spray the lawn with an herbicide, such as a product containing glyphosate.
+<br/>
+Apply according to product directions, wait 10 days and then re-spray any areas that are still green. Wait another 10 days to make sure that all of the turf is dead. Mow off the dead grass and rake it up. If the dead layer is too thick for raking, remove it with a sod cutter. After the area is clear of dead grass and you've graded the site, you can seed the lawn.`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjkKfWPUE9EtaVSxo8Gn9Ld6G3mIYG-7vsyPJl_C_ojIlicA1AE3ZkqoQAmtnCgat7IScQsYVDjKiWkZK2CaQdEUwHNZzmQ8KqHfWtrXfRWV8aZVdwMsmChhnS52PP4VgXcwk_v4mzDD28/s640/2013-09-13_10-35-16_298.jpg",
+  },
+  {
+    id: 5,
+    title: "When is the right time of year to plant new grass seeds",
+    content: `
+Choose the right time of year to plant new grass.
+<br/>
+The time of year you plant grass seed has a direct effect on seeding success. Proper timing ensures that the grass effectively germinates, grows quickly and remains healthy while establishing.
+<br/>
+Early fall is the ideal time to plant grass seed, according to the University of Massachusetts Extension Turf Program.1 At this point, the ground is still warm enough to hasten germination, while the days are cool and sometimes rainy, helping to ensure that the seeds don't dry out. In early fall there is also still sufficient sunlight to allow new grass to thrive and become established before winter arrives.
+<br/>
+Spring seeding is your second best option. Aim to seed early in the season, before the weather warms up substantially. Spring also tends to bring periods of sunshine and rain, both of which contribute to strong growth.`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjF0wGbHf2IunzEbKYNiLqynDVu54UFvkTg75A4EB6mDf1B9KpSRyudopjoP_NKuz22GG5iROaIGhxx7xrgWwUEoI25jxq2PRSpf8KWUFrfynktt2nCLR7XYQaYv6X3oX-OOsoBGNPoTb4/s640/bigstock-Newly-Planted-Grass-Seeds-Star-24542309.jpg",
+  },
+  {
+    id: 6,
+    title: " Fast recovery of your lawn ",
+    content: `
+Lawns face nuisances such as inclement weather, pests, and disease all the time.
+<br/>
+The key to overcoming these inevitable annoyances is to have a lawn that is well-maintained in the first place.
 
+A lawn that receives regular mowing and overall maintenance will recover much more quickly than an unhealthy one. The longer your lawn goes without love and care, the harder it is for your lawn to bounce back to tip-top shape.`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDFa_3Y45ILh27J7iOJMHRT58l6r3Zfqfv4m55HjRPf_St8bYPeP8GLe-LR1IMrACUhVe0WJ-vFCFT1iMwfX5SiDEbJilEpOqyk8NOrwsfnZBp-haONf6DKv4jfbrjub-Unyp0BIBqSpQ/s640/lawn-services-eden-prairie.jpg",
+  },
+  {
+    id: 7,
+    title: "Even growth in your lawn ",
+    content: `
+\bGetting an even growth of your lawn is not an easy task\b
+<br/>
+We’ve all seen yards that are patchy and uneven and I think we can all agree that they can be somewhat of an eyesore.
+<br/>
+Uneven grass growth is what causes this common lawn problem, but fortunately you can avoid this with regular mowing.
+<br/>
+When you slice the growth of your grass down to a short, uniform level on a regular basis, you are actually improving overall growth because there will be an even distribution and absorption of resources that come from the sun and water. Consistency in growth is crucial in maintaining a beautiful yard.
+<br/>
+Call us for more information on how to get a beautiful, even growth in your lawn.`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjmNMubi6SmHDMyDMeakb89aVcBrxdkXM-z6GrfDk4ImtEc9KGKcNhixcOyyZRrglTDJfVccWyikS9_L_tjuXCem2Lv3l1Uzl4FYZOQFj2s1jobj7iCLPKgls4Z7NcCen8vMf3069N-SL8/s640/dark-side-of-the-american-lawn.jpg",
+  },
+  {
+    id: 8,
+    title: " Stronger Grass  ",
+    content: `
+You don’t want a yard full of weaklings do you? We didn’t think so.
+<br/>
+To get the strongest lawn possible, get it mowed as often as needed to keep it at around 3 inches in height.
+<br/>
+When your grass is cut, the healthiest grass shoots will flourish while the weak shoots are left behind.
+<br/>
+The more you get your lawn cut, the more healthy grass shoots you will have which results in a more lush and healthy lawn over time.
+<br/>
+If you need more information, please call us.`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjWm3vMTF7hkVMw_x_JloTliIFYIQejWKuEQVmPYoYs9Z6l5-dMYKbPC2WmisCE2CUTP_Q73AbEeZ_DcjsnTw2VS-kZWrVq3CQoSHL-2VqpBuZLz_7_U0bT6Cm7o6mOUe20CIbpT-_N0vg/s640/511037_49348393.jpg",
+  },
+  {
+    id: 9,
+    title: "  Lawn Care Maintenance & Tips - Caring for your Grass  ",
+    content: `
+When you request an estimate to mow your lawn, you should ask for a detailed quote from the lawn care provider that includes:
+<br/>
+   \b1.\b Labor Cost estimate to mow your lawn
+   \b2.\b Type of Mower to be used
+   \b3.\b Job Duration, hours and minutes
+   \b4.\b References and review links
+<br/>
+<br/>
+<br/>
+<br/>
+\bThe lawn job should should also include the following:\b
+<br/>
+    \b1.\b Advanced planning if required  
+    \b2.\b Trimming and edging
+    \b3.\b The equipment and materials needed 
+    \b4.\b Area preparation and protection
+    \b5.\b Setup and cleanup
+`,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEipqoq3omwi_XZd3m6hbsPDY3ywE8ch52dkgcbjCCkiTrWQ3e6oTQ-LyKPijhb-wH9dC1zAJSmm1fH5kZ-KtZXKKiOIbmZCzr3MpNCWLnRpt1xBCqdW_tUL4FAx_GkRennuhj4vwwCozUg/s1200/ThinkstockPhotos-149360161.jpg",
+  },
   // Add more tips as needed...
 ];
